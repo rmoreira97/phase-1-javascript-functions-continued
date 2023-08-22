9aec1d7373138b1d7958434f7cd94ed1f42519f0
Rafael Moreira <rafa.siffert@gmail.com>
2023-08-22 12:40:33 -0400

    new

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+// code your solution here
+function saturdayFun(activity) {
+    // check if activity is defined
+    if (activity === undefined) {
+        activity = "roller-skate";
+    }
+    
+    
+    return "This Saturday, I want to " + activity + "!" ;
+    
+    }   
+    
+    function mondayWork(activity) { 
+        if (activity === undefined) {
+            activity = "go to the office";
+        }
+        return "This Monday, I will " + activity + ".";
+    }
+    
+    
+    function wrapAdjective(flair = "*") {
+        return function(adjective = "special") {
+            return `You are ${flair}${adjective}${flair}!`
+        }
+    }
+    
+    const encouragingPromptFunction = wrapAdjective("!!!")
